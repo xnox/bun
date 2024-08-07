cc0ab31cfddb378cb1d7d2b8d6f5eb9af28943e7
Ashcon Partovi <ashcon@partovi.net>
2024-08-07 12:53:55 -0700

    Test unified brotli ar

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1173,7 +1173,7 @@ async function buildBoringSsl(options) {
  * @param {BuildOptions} options
  * @returns {string[]}
  */
-function getBrotliArtifacts(options) {
+function getBrotliOriginalArtifacts(options) {
   const { os } = options;
   if (os === "windows") {
     return ["brotlicommon.lib", "brotlidec.lib", "brotlienc.lib"];
@@ -1181,19 +1181,39 @@ function getBrotliArtifacts(options) {
   return ["libbrotlicommon.a", "libbrotlidec.a", "libbrotlienc.a"];
 }
 
+function getBrotliArtifacts(options) {
+  const { os } = options;
+  if (os === "windows") {
+    return ["brotli.lib"];
+  }
+  return ["libbrotli.a"];
+}
+
 /**
  * @param {BuildOptions} options
  */
 async function buildBrotli(options) {
+  const { os, buildPath, ar, ranlib } = options;
   await cmakeGenerateBuild(
-    { ...options, pic: true },
-    "-DBROTLI_BUNDLED_MODE=ON",
+    options,
     "-DBUILD_SHARED_LIBS=OFF",
     "-DBROTLI_BUILD_TOOLS=OFF",
     "-DBROTLI_DISABLE_TESTS=ON",
     "-DBROTLI_EMSCRIPTEN=OFF",
   );
-  await cmakeBuild(options);
+  await cmakeBuild(options, "brotlicommon", "brotlidec", "brotlienc");
+  const objectList = [];
+  for (const artifact of getBrotliOriginalArtifacts(options)) {
+    const artifactPath = join(buildPath, artifact.replace(".", "-"));
+    mkdir(artifactPath, { clean: true });
+    await spawn(ar, ["-x", join(buildPath, artifact)], { cwd: artifactPath });
+    objectList.push(...listFiles(artifactPath).map(filename => join(artifactPath, filename)));
+  }
+  const brotliPath = join(buildPath, os === "windows" ? "brotli.lib" : "libbrotli.a");
+  await spawn(ar, ["-r", brotliPath, ...objectList], { cwd: buildPath });
+  if (ranlib) {
+    await spawn(ranlib, [brotliPath], { cwd: buildPath });
+  }
 }
 
 /**
